9ba2d53d1b0855e7a6b86526518ea3f624f10890
frobian21 <frobian21@yahoo.co.uk>
2019-09-24 12:02:42 +0100

    Conditionals1 Exercise

---
+++ src/main/scala/Day1/Conditionals2.scala
@@ -0,0 +1,14 @@
+package Day1
+
+object Conditionals2 extends App{
+  def sum(a: Int, b: Int, bool: Boolean):Int = {
+    if(a == 0) b
+    else if(b==0) a
+          else if(bool) {a+b}
+          else {a*b}
+  }
+  println(s"sum(2,0,true) is ${sum(2,0,true)}")
+  println(s"sum(0,3,false) is ${sum(0,3,false)}")
+  println(s"sum(2,3,true) is ${sum(2,3,true)}")
+  println(s"sum(2,3,false) is ${sum(2,3,false)}")
+}

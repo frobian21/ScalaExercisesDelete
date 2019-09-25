25a7206237bb3b61b285bd240f5142c4fe48fadd
frobian21 <frobian21@yahoo.co.uk>
2019-09-25 16:34:33 +0100

    Broken Keyboard extra Exercise

--- src/main/scala/Day2/BrokenKeyboard.scala
+++ src/main/scala/Day2/BrokenKeyboard.scala
@@ -6,14 +6,15 @@ object BrokenKeyboard extends App{
   //println(wordList(1))
   def lineReader(): Unit ={
     try{
-    println("=============================\nGive number of lines to read:")
-    val linesToRead=scala.io.StdIn.readInt()
-    val input = new Array[String](linesToRead)
-    for (i <- 0 until linesToRead) input(i)=scala.io.StdIn.readLine().replaceAll("[^a-zA-Z]","").toLowerCase()
-      //filtering input, not necessary.
-    input.map(println(_))
-    println("================\nLongest word(s):")
-      input.map(filterWordList(_))
+      println("=============================\nGive number of lines to read:")
+      val linesToRead=scala.io.StdIn.readInt()
+      val input = new Array[String](linesToRead)
+      for (i <- 0 until linesToRead) input(i)=scala.io.StdIn.readLine().replaceAll("[^a-zA-Z]","").toLowerCase()
+        //filtering input, not necessary.
+      input.map(println(_))
+      println("================\nLongest word(s):")
+      //time it
+      input.map(time{filterWordList(_)})
     } catch {
       case e: Error => println(e)
       case _ => println("Something went wrong")
@@ -21,6 +22,14 @@ object BrokenKeyboard extends App{
   }
   def filterWordList(str: String)={
     println(wordList.filter(_ matches s"([$str]+)").foldLeft("")((a: String, b: String) =>{if(a.length > b.length) a else b}))
+    println(wordList.filter(_.toSet == str.toSet).foldLeft("")((a: String, b: String) =>{if(a.length > b.length) a else b}))
+  }
+  def time[R](block: => R): R = {
+    val t0 = System.nanoTime()
+    val result = block    // call-by-name
+    val t1 = System.nanoTime()
+    println("Elapsed time: " + (t1 - t0) + "ns")
+    result
   }
   lineReader()
 }

b400d34b38c14181c21f1242ae5944edc086da58
frobian21 <frobian21@yahoo.co.uk>
2019-09-26 13:03:52 +0100

    Garage stage 2 complete

--- src/main/scala/Day2/Garage/Stage1.scala
+++ src/main/scala/Day2/Garage/Stage1.scala
@@ -8,11 +8,29 @@ object Stage1 extends App{
   class Customer(name: String) extends Person(name: String){
     override def toString() = s"Customer($name)"
   }
-  abstract case class Vehicle(ID: String, regNo: String, owner: Customer)
-  class Car(ID: String, regNo: String, owner: Customer) extends Vehicle(ID: String, regNo: String, owner: Customer){
-    override def toString() = s"Car($ID,$regNo,$owner)"
+  abstract case class Vehicle(){
+    val ID: String
+    val regNo: String
+    val make: String
+    var isFixed: Boolean
+    var days: Int
+    val owner: Customer
   }
-  class Bike(ID: String, regNo: String, owner: Customer) extends Vehicle(ID: String, regNo: String, owner: Customer){
-    override def toString() = s"Bike($ID,$regNo,$owner)"
+  object Vehicle{
+    var ID = 100
+    def generateID():String ={
+      ID += 1
+      ID.toString
+    }
+  }
+
+  class Car(val ID: String, val regNo: String, val make: String, var isFixed: Boolean = false, var days: Int = 1, val owner: Customer) extends Vehicle {
+    def this(regNo: String, make: String, owner: Customer) = this(Vehicle.generateID(), regNo, make, false, 1, owner)
+    override def toString() = s"Car(ID $ID,regNo $regNo,make $make,isFixed $isFixed,days $days,owner $owner)"
+  }
+
+  class Bike(val ID: String, val regNo: String, val make: String, var isFixed: Boolean = false, var days: Int = 1, val owner: Customer) extends Vehicle {
+    def this(regNo: String, make: String, owner: Customer) = this(Vehicle.generateID(), regNo, make, false, 1, owner)
+    override def toString() = s"Bike(ID $ID,regNo $regNo,make $make,isFixed $isFixed,days $days,owner $owner)"
   }
 }
---
+++ src/main/scala/Day3/Garage/Stage3.scala
@@ -0,0 +1,7 @@
+package Day3.Garage
+
+import Day3.Garage.Stage2._
+
+object Stage3 extends App{
+
+}

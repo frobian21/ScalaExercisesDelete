435ab6c967be654f9793975b77b411c768944e64
frobian21 <frobian21@yahoo.co.uk>
2019-09-27 11:20:27 +0100

    LumberJack exercise refactor

--- src/main/scala/Day4/Lumberjack.scala
+++ src/main/scala/Day4/Lumberjack.scala
@@ -3,25 +3,25 @@ package Day4
 object Lumberjack extends App{
    import scala.io.StdIn
   println("=============================\nGrid size:")
-  val gridSize:Int=StdIn.readLine().replaceAll("[^\\s+\\d+]", "").split("[\\s+]")
-    .map(str => str=="" match {
-      case false => Some(Integer.parseInt(str))
-      case _ => None
-    }).head.getOrElse(3)
+  val gridSize:Int=getMeAnIntOrElse(3)
   println("=============================\nNumber of Logs:")
-  val numberOfLogs:Int=StdIn.readLine().replaceAll("[^\\s+\\d+]", "").split("[\\s+]")
-    .map(str => str=="" match {
-      case false => Some(Integer.parseInt(str))
-      case _ => None
-    }).head.getOrElse(7)
+  val numberOfLogs:Int=getMeAnIntOrElse(7)
   val grid = Array.ofDim[Int](gridSize, gridSize)
   println("Current arrangement of grid:")
-  for(i <- 0 until gridSize) {
+  for(i <- 0 until grid.length) {
     val line = StdIn.readLine().trim.split("[,\\s+]")
-    for(j <- 0 until math.min(gridSize,line.length)) grid(i)(j) = Integer.parseInt(line(j))
+    for(j <- 0 until math.min(grid.length,line.length)) grid(i)(j) = Integer.parseInt(line(j))
   }
   (1 to numberOfLogs).foreach(_ => grid(minIndex()._1).update(minIndex()._2, minIndex()._3))
-  printGrid(gridSize,grid)
+  printGrid(grid)
+
+  def getMeAnIntOrElse(_else:Int):Int={
+    StdIn.readLine().replaceAll("[^\\s+\\d+]", "").split("[\\s+]")
+      .map(str => str=="" match {
+        case false => Some(Integer.parseInt(str))
+        case _ => None
+      }).head.getOrElse(_else)
+  }
 
   def minIndex():(Int, Int, Int)={
     val minimum = grid.flatten.min
@@ -29,12 +29,10 @@ object Lumberjack extends App{
     (flatIndex/gridSize, flatIndex%gridSize, minimum+1)
   }
 
-  def printGrid(gridSize: Int, grid: Array[Array[Int]])={
-    for(i <- 0 until gridSize) {
+  def printGrid(grid: Array[Array[Int]])={
+    for(i <- 0 until grid.length) {
       grid(i).foreach(str => print(s"$str "))
       print("\n")
-//    for (j <- 0 until gridSize) print(s"${grid(i)(j)} ")
-//    print("\n")
     }
   }
 

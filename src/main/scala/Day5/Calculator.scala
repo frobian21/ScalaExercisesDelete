ad64a13ae71280454ad4a5c610028b51285d6f51
frobian21 <frobian21@yahoo.co.uk>
2019-09-30 13:49:18 +0100

    Calculator test for division passed

--- src/main/scala/Day5/Calculator.scala
+++ src/main/scala/Day5/Calculator.scala
@@ -1,17 +1,18 @@
 package Day5
 
 class Calculator {
-  def sum(num1:Int, num2:Int):Int={
-    num1+ num2
+  def sum[T](num1:T, num2:T)(implicit n:Numeric[T])={
+    import n._
+    num1 + num2
   }
-  def subtract(num1:Int, num2:Int):Int= {
-    num1 - num2
+  def subtract[T](num1:T, num2:T)(implicit n:Numeric[T])= {
+    import n._
+    (math rint  (num1 - num2).toDouble * 1000) / 1000
   }
   def multiply(num1:Int, num2:Int):Int= {
     num1*num2
   }
-  def divide(num1:Int, num2:Int):Float= {
-    num1.toFloat/num2
+  def divide(num1:Int, num2:Int):Double= {
+    num1.toDouble/num2
   }
-
 }

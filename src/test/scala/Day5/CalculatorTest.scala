a1805428c0edba5fd89baf21e807ab04b02e7b47
frobian21 <frobian21@yahoo.co.uk>
2019-09-30 13:25:54 +0100

    Calculator tests for subtraction multiply and divide

--- src/test/scala/Day5/CalculatorTest.scala
+++ src/test/scala/Day5/CalculatorTest.scala
@@ -3,13 +3,27 @@ package Day5
 import resources.BaseTestingClass
 
 class CalculatorTest extends BaseTestingClass{
+  val calculator = new Calculator()
   test("Make a calculator instance"){
-    val calculator = new Calculator()
     assert(calculator.isInstanceOf[Calculator]==true)
   }
-  test("Return sum of two numbers with calculator"){
-    val calculator = new Calculator()
+  test("Use calculator to return sum of two numbers"){
     assert(calculator.sum(10, 2) == 12)
     assert(calculator.sum(12, 2) == 14)
   }
+  test("Use calculator to return difference of two numbers"){
+    assert(calculator.subtract(10,8)==2)
+    assert(calculator.subtract(8,10)==-2)
+  }
+  test("Use calculator to return multiple of two numbers"){
+    assert(calculator.multiply(10,8)==80)
+    assert(calculator.multiply(8,10)==80)
+    assert(calculator.multiply(7,5)==35)
+  }
+
+  test("Use calculator to return division of two numbers"){
+    assert(calculator.divide(10,5)==2)
+    assert(calculator.divide(5, 10)==0.5)
+  }
+
 }

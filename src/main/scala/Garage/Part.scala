0c48d94991531ac4a51e170d3becf795d54f7552
frobian21 <frobian21@yahoo.co.uk>
2019-10-03 10:33:17 +0100

    Part refactor

--- src/main/scala/Garage/Part.scala
+++ src/main/scala/Garage/Part.scala
@@ -5,20 +5,19 @@ import scala.util.Random
 case class Part(name: String, var isBroken: Boolean, var hoursToFix: Int = 0)
 
 object Part{
-  val acceptedParts = Set[String]("door", "bonnet", "window", "engine", "tires", "seat", "bumper",
+  val acceptedParts:Set[String] = Set("door", "bonnet", "window", "engine", "tires", "seat", "bumper",
     "exhaust", "radio", "headlights", "warning lights", "mirrors", "windscreen wipers", "airbag", "brakes")
   def generatePartList(numberOfParts:Int=10):Array[Part]={
     var partList= List[Part]()
-    (0 until numberOfParts).foreach(_ => partList ::= generatePart(getPartSet()))
+    (0 until numberOfParts).foreach(_ => partList ::= generate(getPartSet))
 
-    def getPartSet():Set[String]= acceptedParts--partList.collect{ case part:Part=>part.name}
+    def getPartSet:Set[String]= acceptedParts--partList.collect{ case part:Part=>part.name}
 
-    def generatePart(nameSet:Set[String]):Part= {
+    def generate(nameSet:Set[String]):Part= {
       val chosenName = nameSet.toVector(Random.nextInt(nameSet.size))
-      Random.nextFloat() > 0.5 match {
-        case true => Part(chosenName, true, Random.between(1, 4))
-        case false => Part(chosenName, false, 0)
-      }
+      val isBroken = Random.nextFloat() > 0.5
+      if (isBroken) Part(chosenName, isBroken, Random.between(1, 4))
+      else Part(chosenName, isBroken)
     }
     partList.toArray
   }
